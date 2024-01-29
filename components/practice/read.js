be31efe765758a66847a475245a0fede8134672b
Guilhem Malfre <guilhem@metroworks.co.jp>
2024-01-29 17:47:28 +0800

    Moved music functions from component to separate library.

--- components/practice/read.js
+++ components/practice/read.js
@@ -1,6 +1,6 @@
 import store from '../../store.js';
 // import ABCJS from '../../vendor/abcjs-basic-min.js'
-
+import Music from '../../lib/music.js';
 
 export default {
     name: 'Read Practice Component',
@@ -17,63 +17,6 @@ export default {
             //$noteContainer.value.innerText = 'Dynamically loaded';
         });
 
-        const Music = {
-            /**
-             * taking the "middle C" as 0, so that common notes will have easy numbers, and to gracefully handle pianos of various widths
-             * 88 notes: [-39, 48] i.e. A,,,, and c''' (actual center being EF...)
-             * 49 notes: [-24, 24]
-             *
-             * Call samples:
-             * randomNote(0, 11) returns a random note in the octave of the middle C
-             * randomNote(11) returns a random note within 11 notes of the middle C, i.e. the same as randomNote(-11, 11)
-             * randomNote() ... not sure yet
-             */
-            randomPitch: function(range1, range2) {
-                // const sampleNotes = ["X:1\nT:Example\nK:Bb\nBcde|\n", "X:1\nK:D\nDD AA|BBA2|\n"];
-                let min = -39, max = 48; // piano extremes
-                if (range1 != undefined) {
-                    if (range2 != undefined) {
-                        min = range1;
-                        max = range2;
-                    } else {
-                        min = -range1;
-                        max = range1;
-                    }
-                }
-                let randomNoteInRange = Math.floor(Math.random() * (max - min + 1)) + min;
-                return this.numToAbc(randomNoteInRange);
-            },
-            numToAbc: function(noteNum) {
-                const noteInOctave = (noteNum + 120) % 12; // +120 to avoid negative modulo
-                const octave = Math.floor(noteNum / 12);
-                const sharp = [1, 3, 6, 8, 10].indexOf(noteInOctave);
-
-                let adjustedNote = noteInOctave;
-                for (const i of [1, 3, 6, 8, 10]) {
-                    if (noteInOctave >= i) adjustedNote--;
-                }
-
-                const aIndexed = (adjustedNote + 2 + 700) % 7; // +2 to go from C=0 to A=1, +700 to make sure we never get a negative modulo
-                let abcNote = String.fromCharCode(97 + aIndexed);
-                // adjusting for octaves: upper / lower case
-                if (noteNum < 12) abcNote = abcNote.toUpperCase();
-                // adjusting further: ' and ,
-                for (let i = 1; i < octave; i++) {
-                    abcNote += "'";
-                }
-                for (let i = 0; i > octave; i--) {
-                    abcNote += ",";
-                }
-
-                if (sharp != -1) {
-                    abcNote = '^' + abcNote;
-                    // need to use _ instead somehow, but how to decide? Based on key signature?
-                }
-
-                // console.log({noteNum, noteInOctave, octave, sharp, adjustedNote, abcNote});
-                return abcNote;
-            },
-        };
         function pickRandomNote() {
             // $noteContainer.value.innerText = 'Dynamically loaded'; // could display the SVG here but ~~~~
             // const rendered = ABCJS.renderAbc("*", note); // not sure how to get the generated SVG from that
