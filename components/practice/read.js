57d123b372b189f64e39addcbfc385fdefe95c21
chyro <guilhem.malfre@gmail.com>
2024-02-08 21:04:26 +0800

    Practice component updates
    - modified Note to use piano key number as reference
    - practice component now uses the Math2 weighted random generator

--- components/practice/read.js
+++ components/practice/read.js
@@ -1,6 +1,6 @@
 import store from '../../store.js';
 // import ABCJS from '../../vendor/abcjs-basic-min.js'
-import * as Music from '../../lib/music.js';
+import {Math2, Memory, Note} from '../../lib/music.js';
 
 export default {
     name: 'Read Practice Component',
@@ -10,29 +10,49 @@ export default {
         const {watchEffect, onMounted, ref} = Vue;
 
         const name = 'Read Practice Component';
+        const exerciseType = 'Read Practice';
 
         const noteContainer = ref(null);
         const visualInput = ref(null);
         let testNote;
+
+        /**
+         * Practice settings
+         *
+         * TODO: get those as component param, from the upcoming Dean class
+         */
+        const keySignature = 'C';
+        const keyRange = [1, 88];
+        const notesToPractice = {};
+        for (let i of Array(24).keys()) notesToPractice[i + 40] = 5; // essentially the whole treble clef - all notes equally likely to come up
+        notesToPractice[46] = 25; // except F# which will come up 5 times more, to see if that works
+
+        const notePicker = Math2.getRandomRotator(notesToPractice);
 
         onMounted(() => {
             visualInput.value.addEventListener('press', keyPressed);
         });
 
         function keyPressed(e) {
-            console.log('piano-keyboard is saying somebody pressed key #' + e.detail.key);
-            console.log('Test note was ' + testNote.num + ' which on a full piano would be key #' + (testNote.num + 40));
+            const questionKey = testNote.pianoKey;
+            const answerKey = e.detail.key;
+            console.log('piano-keyboard is saying somebody pressed key #' + answerKey + ', test note was ' + questionKey);
+            if (answerKey == questionKey) {
+                Memory.registerCorrect(exerciseType, questionKey); // TODO: also note the time taken to answer
+                // TODO: generate the next test note
+            } else {
+                Memory.registerError(exerciseType, questionKey, answerKey);
+            }
         }
 
         function pickRandomNote(e) {
             // noteContainer.value.innerText = 'Dynamically loaded'; // could display the SVG here but ~~~~
             // const rendered = ABCJS.renderAbc("*", note); // not sure how to get the generated SVG from that
 
-            let key = 'C'; // eventually based on practice settings
-            testNote = Music.randomNote(0, 23); // essentially the whole treble clef
-            // for (let i = -39; i < 49; i++) { testNote += Music.numToAbc(i); } // full piano keyboard, left to right
-            let abcNoteSheet ="X:1\nL:1/4\nK:" + key + "\n" + testNote.abc;
-            // ABCJS.renderAbc('note-container', sampleNotes[1]);
+            testNote = notePicker.pick();
+            testNote = Note.fromPianoKey(testNote);
+            let abcNoteSheet ="X:1\nL:1/4\nK:" + keySignature + "\n" + testNote.abc;
+            // for (let i = 1; i < 88; i++) { abcNoteSheet += Note.fromPianoKey(i).abc; } // full piano keyboard, left to right
             ABCJS.renderAbc('note-container', abcNoteSheet);
 
             // TODO: init elsewhere
@@ -53,7 +73,7 @@ export default {
             // libs
             store,
             // vars
-            name, testNote,
+            name, exerciseType, testNote, keyRange, notePicker,
             // listeners
             pickRandomNote, keyPressed,
             // refs
@@ -71,10 +91,10 @@ export default {
             <div ref="noteContainer" id="note-container"></div>
         </div>
         <div class="answer">
-            <p>TODO: display keyboard as webcomponent, event listener checking, generate next</p>
-            <piano-keyboard ref="visualInput" from=1 to=88 interactive=1></piano-keyboard>
+            <p class="class{{keyRange[0]}}">{{keyRange[0]}}-{{keyRange[1]}}</p>
+            <piano-keyboard ref="visualInput" :from="keyRange[0]" :to="keyRange[1]" interactive=1></piano-keyboard>
             <p>TODO also: detect midi input</p>
-            <button v-on:click="pickRandomNote">Click</button>
+            <button v-on:click="pickRandomNote">Start (display a random note)</button>
         </div>
       </div>
     `,
